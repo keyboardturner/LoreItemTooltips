f647ded29266f172d2a26ee6cca0e5fd84b4385b
keyboardturner <20036792+keyboardturner@users.noreply.github.com>
2021-05-24 02:14:47 -0700

    Update LoreItemTooltips.lua
    updated credit count

--- LoreItemTooltips.lua
+++ LoreItemTooltips.lua
@@ -91,7 +91,7 @@ core.commands = {
 	end,
 
 	["credits"] = function()
-		print(LitText .. "Special thanks to the following people who helped contribute to this project!" .. "\n|cffffe880Rheä-WyrmrestAccord(US)|r - 112 items")
+		print(LitText .. "Special thanks to the following people who helped contribute to this project!" .. "\n|cffffe880Rheä-WyrmrestAccord(US)|r - 229 items")
 	end,
 
 	["credit"] = function()
